5c11098a5990d3d824821549517abe4a45665d6c
YellowTulipShow <main@yellowtulipshow.site>
2022-07-01 12:10:56 +0800

    合并

--- src/CodingSupportLibrary/UseTestExtend.cs
+++ src/CodingSupportLibrary/UseTestExtend.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -10,72 +9,46 @@ namespace CodingSupportLibrary
     /// </summary>
     public static class UseTestExtend
     {
+        private const char space = ' ';
+        private const int single_line_byte_number = 20;
+        private const int single_byte_length = 3;
         public static string GetWriteLogFileContentBytesContent(this FileInfo codeFile)
         {
             StringBuilder str = new StringBuilder();
             str.AppendLine($"文件: {codeFile.FullName}");
             byte[] datas = File.ReadAllBytes(codeFile.FullName);
             str.AppendLine($"名称: {codeFile.Name}");
-
-            str.AppendLine($"HEX 十六进制:");
-            str.Append(WriteLogFileContentBytes_HEX(datas));
-            str.AppendLine($"OCT 十进制:");
-            str.Append(WriteLogFileContentBytes_OCT(datas));
-
+            str.AppendLine($"\nHEX 十六进制:");
+            str.Append(GetWriteLogFileContentBytesContent_StringBuilder(datas, b => b.ToString("X2")));
+            str.AppendLine($"\nOCT 十进制:");
+            str.Append(GetWriteLogFileContentBytesContent_StringBuilder(datas, b => b.ToString()));
+            str.Append("\n");
             return str.ToString();
         }
-        private static StringBuilder WriteLogFileContentBytes_HEX(byte[] datas)
+        private static StringBuilder GetWriteLogFileContentBytesContent_StringBuilder(byte[] datas, Func<byte, string> byteToStrFunc)
         {
             StringBuilder str = new StringBuilder();
-            const char space = ' ';
-            const int region = 20;
-            int header_len = (datas.Length + region).ToString().Length;
+            int header_len = (datas.Length + single_line_byte_number).ToString().Length;
             string get_line_header(int line)
             {
                 return $"  {line.ToString().PadLeft(header_len, space)}:  ";
             };
             str.Append($"{"".PadLeft(get_line_header(0).Length)}");
             string item_space = "".PadLeft(2, space);
-            for (int i = 0; i < region; i++)
+            for (int i = 0; i < single_line_byte_number; i++)
             {
-                str.Append($"{i,3}{item_space}");
+                string end_join = i == single_line_byte_number - 1 ? "" : item_space;
+                str.Append($"{i,single_byte_length}{end_join}");
             }
             for (int i = 0; i < datas.Length; i++)
             {
-                if (i % region == 0)
+                if (i % single_line_byte_number == 0)
                 {
-                    int line = (int)Math.Ceiling((decimal)(i / region)) * region;
+                    int line = (int)Math.Ceiling((decimal)(i / single_line_byte_number)) * single_line_byte_number;
                     str.Append($"\n{get_line_header(line)}");
                 }
-                str.Append($"{datas[i],3:X2}{item_space}");
-            }
-            str.AppendLine(string.Empty);
-            return str;
-        }
-        private static StringBuilder WriteLogFileContentBytes_OCT(byte[] datas)
-        {
-            StringBuilder str = new StringBuilder();
-            const char space = ' ';
-            const int region = 20;
-            int header_len = (datas.Length + region).ToString().Length;
-            string get_line_header(int line)
-            {
-                return $"  {line.ToString().PadLeft(header_len, space)}:  ";
-            };
-            str.Append($"{"".PadLeft(get_line_header(0).Length)}");
-            string item_space = "".PadLeft(2, space);
-            for (int i = 0; i < region; i++)
-            {
-                str.Append($"{i,3}{item_space}");
-            }
-            for (int i = 0; i < datas.Length; i++)
-            {
-                if (i % region == 0)
-                {
-                    int line = (int)Math.Ceiling((decimal)(i / region)) * region;
-                    str.Append($"\n{get_line_header(line)}");
-                }
-                str.Append($"{datas[i],3}{item_space}");
+                string end_join = (i + 1) % single_line_byte_number == 0 ? "" : item_space;
+                str.Append($"{byteToStrFunc(datas[i]),single_byte_length}{end_join}");
             }
             str.AppendLine(string.Empty);
             return str;
