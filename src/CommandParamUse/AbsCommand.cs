3943a4ba60c12c8e10a79cc796e06c71b3d20b78
YellowTulipShow <main@yellowtulipshow.site>
2022-07-10 13:28:24 +0800

    描述

--- src/CommandParamUse/AbsCommand.cs
+++ src/CommandParamUse/AbsCommand.cs
@@ -6,10 +6,19 @@ using YTS.Log;
 
 namespace CommandParamUse
 {
+    /// <summary>
+    /// 抽象类: 基础命令
+    /// </summary>
     public abstract class AbsCommand
     {
         private readonly ILog log;
         private readonly ICommand parent;
+
+        /// <summary>
+        /// 实例化基础命令
+        /// </summary>
+        /// <param name="log">日志接口</param>
+        /// <param name="parent">父级命令</param>
         public AbsCommand(ILog log, ICommand parent)
         {
         }
