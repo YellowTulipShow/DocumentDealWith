0c2ab622eab4e890c7b6dcfb85e201cbd11aa55b
YellowTulipShow <main@yellowtulipshow.site>
2022-07-13 11:58:39 +0800

    暂存测试

--- src/DocumentDealWithCommand.Logic.Test/TestChangeFileNameHelp.cs
+++ src/DocumentDealWithCommand.Logic.Test/TestChangeFileNameHelp.cs
@@ -3,15 +3,18 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 using YTS.Log;
+using YTS.ConsolePrint;
 
 using DocumentDealWithCommand.Logic.Implementation;
+using DocumentDealWithCommand.Logic.Models;
 
 namespace DocumentDealWithCommand.Logic.Test
 {
     [TestClass]
-    public class TestChangeFileNameHelp
+    public class TestChangeFileNameHelp : IPrintColor
     {
         private ILog log;
         [TestInitialize]
@@ -32,18 +35,61 @@ namespace DocumentDealWithCommand.Logic.Test
         {
             Encoding editEncoding = Encoding.UTF8;
             var help = new ChangeFileNameHelp(log);
-            void Test(string sourceFileName, string targetFileName)
-            {
-                string expectedFilePath = $"./changeFileNameDire/{sourceFileName}.txt";
-                FileInfo codeFile = new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                    $"./changeFileNameDire/{sourceFileName}.txt"));
-                if (!codeFile.Directory.Exists)
-                    codeFile.Directory.Create();
-                string text = expectedFilePath;
-                File.WriteAllText(codeFile.FullName, text, editEncoding);
-            }
 
-            Assert.IsTrue(false);
+            DirectoryInfo rootDire = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory,
+                $"./changeFileNameDire"));
+            if (!rootDire.Exists)
+                rootDire.Create();
+            var r = new Random();
+            string chars = "0123456789qwertyuiopasdfghjklzxcvbnm_-";
+            char getChar() {
+                return chars[r.Next(0, chars.Length)];
+            }
+            int add_count = r.Next(5, 30);
+            for (int i = 0; i < add_count; i++)
+            {
+                string name = $"{getChar()}{getChar()}_{i}.txt";
+                FileInfo addFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, name));
+                File.WriteAllText(addFile.FullName, name, editEncoding);
+            }
+            FileInfo[] existsFiles = rootDire.GetFiles();
+
+            IList<HandleRenameResult> rlist = new List<HandleRenameResult>();
+            IDictionary<string, string> dict_record = new Dictionary<string, string>();
+            for (int i = 0; i < existsFiles.Length; i++)
+            {
+                FileInfo item = existsFiles[i];
+                string target = $"{getChar()}{getChar()}_{i}.txt";
+                rlist.Add(new HandleRenameResult()
+                {
+                    Source = item,
+                    Result = target,
+                });
+                dict_record[target] = item.Name;
+            }
+            int execute_sign = help.ChangeFileName(null, rootDire, rlist);
+            Assert.AreEqual(0, execute_sign);
+        }
+
+        public void Write(string content, EPrintColor textColor, EPrintColor backgroundColor)
+        {
+        }
+
+        public void WriteLine(string content, EPrintColor textColor, EPrintColor backgroundColor)
+        {
+        }
+
+        public int GetLineCount()
+        {
+            return 0;
+        }
+
+        public void Write(string content)
+        {
+        }
+
+        public void WriteLine(string content)
+        {
         }
     }
 }
