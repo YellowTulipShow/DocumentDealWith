8bdb591bab58ea944148b803417f72598176ef01
YellowTulipShow <main@yellowtulipshow.site>
2022-08-19 16:49:53 +0800

    修改

--- src/DocumentDealWithCommand.Test/OverrideClass/Test_MainCommand.cs
+++ src/DocumentDealWithCommand.Test/OverrideClass/Test_MainCommand.cs
@@ -9,98 +9,118 @@ using YTS.Log;
 
 namespace DocumentDealWithCommand.Test.OverrideClass
 {
-
-
+    public interface ITestAllExecute :
+        IExecute<CommandParameters_Content_Encoding>,
+        IExecute<CommandParameters_Content_NewLine>,
+        IExecute<ParamRenameWhole>,
+        IExecute<ParamRenameAddOrDelete>,
+        IExecute<ParamRenameReplace>
+    {
+    }
 
     /// <inheritdoc/>
     public class Test_MainCommand : MainCommand
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_MainCommand(ILog log) : base(log) { }
+        public Test_MainCommand(ILog log, ITestAllExecute exe) : base(log)
+        {
+            this.exe = exe;
+        }
 
         public override IEnumerable<ICommandSub> GetSubCommands()
         {
-            yield return new Test_SubCommand_Content(log);
-            yield return new Test_SubCommand_Rename_Whole(log);
+            yield return new Test_SubCommand_Content(log, exe);
+            yield return new Test_SubCommand_Rename_Whole(log, exe);
         }
     }
 
     /// <inheritdoc/>
     public class Test_SubCommand_Content : SubCommand_Content
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Content(ILog log) : base(log) { }
+        public Test_SubCommand_Content(ILog log, ITestAllExecute exe) : base(log)
+        {
+            this.exe = exe;
+        }
 
         public override IEnumerable<ICommandSub> GetSubCommands()
         {
-            yield return new Test_SubCommand_Content_Encoding(log);
-            yield return new Test_SubCommand_Content_NewLine(log);
+            yield return new Test_SubCommand_Content_Encoding(log, exe);
+            yield return new Test_SubCommand_Content_NewLine(log, exe);
         }
     }
 
     /// <inheritdoc/>
     public class Test_SubCommand_Content_Encoding : SubCommand_Content_Encoding
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Content_Encoding(ILog log) : base(log) { }
-
-        /// <inheritdoc/>
-        public override IExecute<ParamRenameWhole> GetExecute()
+        public Test_SubCommand_Content_Encoding(ILog log, ITestAllExecute exe) : base(log)
         {
+            this.exe = exe;
         }
+
+        public override IExecute<CommandParameters_Content_Encoding> GetExecute() => exe;
     }
     /// <inheritdoc/>
     public class Test_SubCommand_Content_NewLine : SubCommand_Content_NewLine
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Content_NewLine(ILog log) : base(log) { }
-
-        /// <inheritdoc/>
-        public override IExecute<ParamRenameWhole> GetExecute()
+        public Test_SubCommand_Content_NewLine(ILog log, ITestAllExecute exe) : base(log)
         {
+            this.exe = exe;
         }
+
+        public override IExecute<CommandParameters_Content_NewLine> GetExecute() => exe;
     }
 
 
     /// <inheritdoc/>
     public class Test_SubCommand_Rename_Whole : SubCommand_Rename_Whole
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Rename_Whole(ILog log) : base(log) { }
+        public Test_SubCommand_Rename_Whole(ILog log, ITestAllExecute exe) : base(log)
+        {
+            this.exe = exe;
+        }
 
         /// <inheritdoc/>
-        public override IExecute<ParamRenameWhole> GetExecute()
-        {
-        }
+        public override IExecute<ParamRenameWhole> GetExecute() => exe;
 
         /// <inheritdoc/>
         public override IEnumerable<ICommandSub> GetSubCommands()
         {
-            yield return new Test_SubCommand_Rename_Replace(log);
-            yield return new Test_SubCommand_Rename_AddOrDelete(log);
+            yield return new Test_SubCommand_Rename_Replace(log, exe);
+            yield return new Test_SubCommand_Rename_AddOrDelete(log, exe);
         }
     }
     /// <inheritdoc/>
     public class Test_SubCommand_Rename_AddOrDelete : SubCommand_Rename_AddOrDelete
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Rename_AddOrDelete(ILog log) : base(log) { }
-
-        /// <inheritdoc/>
-        public override IExecute<ParamRenameWhole> GetExecute()
+        public Test_SubCommand_Rename_AddOrDelete(ILog log, ITestAllExecute exe) : base(log)
         {
+            this.exe = exe;
         }
+
+        public override IExecute<ParamRenameAddOrDelete> GetExecute() => exe;
     }
 
     /// <inheritdoc/>
     public class Test_SubCommand_Rename_Replace : SubCommand_Rename_Replace
     {
+        private readonly ITestAllExecute exe;
         /// <inheritdoc/>
-        public Test_SubCommand_Rename_Replace(ILog log) : base(log) { }
-
-        /// <inheritdoc/>
-        public override IExecute<ParamRenameWhole> GetExecute()
+        public Test_SubCommand_Rename_Replace(ILog log, ITestAllExecute exe) : base(log)
         {
+            this.exe = exe;
         }
+
+        public override IExecute<ParamRenameReplace> GetExecute() => exe;
     }
 }
